ba8ad5f8da806b829bcdf765338f06ad190a247c
rapiro <rapiromail@gmail.com>
2025-08-23 08:17:43 -0700

    Uplaoding files by POST and responding with JSON with download link

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"github.com/go-file-storage/handlers"
+	"net/http"
+)
+
+func main() {
+	//Uplaod handler
+	http.HandleFunc("/api/upload", handlers.UploadHandler)
+
+	//Download handler
+	http.HandleFunc("/api/download", handlers.DownloadHandler)
+
+	//Health check handler
+	http.HandleFunc("/api/health", handlers.HealthHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
